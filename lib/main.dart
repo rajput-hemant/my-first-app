89e51c0e06e2b49e0e2aeb2e2a6786604d67cae6
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-05-23 19:36:02 +0530

    init
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('My First App!')),
+        body: Text('This is my Default App!'),
+      ),
+    );
+  }
+}

2bbda639e3dbaae09e53a3b96e41f5037eec2ca9
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-05-24 00:20:19 +0530

    Updated question.dart Wrapped Text widget in container & add some properties to Container & Text Widget

--- lib/question.dart
+++ lib/question.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
+// Wrapped Text widget in container & add some properties to Container & Text Widget
+
 class Question extends StatelessWidget {
   final String questionText;
 
   Question(this.questionText);
 
+  @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
+
+//  Version 6 - created question widget to handle questions seperately
+
+// class Question extends StatelessWidget {
+//   final String questionText;
+//   Widget build(BuildContext context) {
+//     return Text(questionText);
+//   }
+// }

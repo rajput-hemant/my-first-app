cb3f83c3cc881d693320b0ea84e8f818b5d051b9
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-05-23 20:06:56 +0530

    Added onPressed fn(s) for RaisedButton
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/main.dart
+++ lib/main.dart
@@ -6,37 +6,74 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-//  Version 2 - Added text and buttons
+//  Version 3 - Added onPressed fn(s) for RaisedButton 
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer 1 Choosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App!'),
+        appBar: AppBar(title: Text('My First App!')),
+        body: Column(
+          children: [
+            Text('The Question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 Choosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                // do something
+                print('Answer 3 Choosen!');
+              },
+            ),
+          ],
         ),
-        body: Column(children: [
-          Text('The Question!'),
-          RaisedButton(
-            child: Text('Answer 1'),
-            onPressed: null,
-          ),
-          RaisedButton(
-            child: Text('Answer 2'),
-            onPressed: null,
-          ),
-          RaisedButton(
-            child: Text('Answer 3'),
-            onPressed: null,
-          ),
-        ]),
       ),
     );
   }
 }
 
-//  Version 1 - Gettinf Started
+//  Version 2 - Added text and buttons
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('My First App!'),
+//         ),
+//         body: Column(children: [
+//           Text('The Question!'),
+//           RaisedButton(
+//             child: Text('Answer 1'),
+//             onPressed: null,
+//           ),
+//           RaisedButton(
+//             child: Text('Answer 2'),
+//             onPressed: null,
+//           ),
+//           RaisedButton(
+//             child: Text('Answer 3'),
+//             onPressed: null,
+//           ),
+//         ]),
+//       ),
+//     );
+//   }
+// }
+
+//  Version 1 - Getting Started
 
 // class MyApp extends StatelessWidget {
 //   @override

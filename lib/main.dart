afb99f5839f7fa045f4e5267b1358c063285ecbb
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-05-23 19:49:36 +0530

    Added body text & buttons
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+//  Version 2 - Added text and buttons
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('My First App!')),
-        body: Text('This is my Default App!'),
+        appBar: AppBar(
+          title: Text('My First App!'),
+        ),
+        body: Column(children: [
+          Text('The Question!'),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: null,
+          ),
+        ]),
       ),
     );
   }
 }
+
+//  Version 1 - Gettinf Started
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('My First App!')),
+//         body: Text('This is my Default App!'),
+//       ),
+//     );
+//   }
+// }
